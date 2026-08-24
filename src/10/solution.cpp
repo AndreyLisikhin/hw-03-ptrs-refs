7c288358fee4701b96512d912443b3539fc25243
AndreyLisikhin <dunylislis@gmail.com>
2026-08-24 23:00:46 +0500

    Task 10

---
+++ src/10/solution.cpp
@@ -0,0 +1,10 @@
+int sum_array(int* from, int* to) {
+	if (from == nullptr || to == nullptr || from >to) {
+		return 0;
+	}
+	int sum = 0;
+	for (int* p = from; p < to;p++) {
+		sum += *p;
+	}
+	return sum;
+}
